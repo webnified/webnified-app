f51cd022b814f7f5ed75c96eeee2cf7513ca07a9
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-25 22:28:05 +0800

    Updated package.json to use server.js Added server.js because fuck openshift servers.

---
+++ server.js
@@ -0,0 +1,12 @@
+var childprocess = require( "child_process" );
+var task = childprocess.exec( "cd ./sails/webnified-app/ && npm start --prod 1> ../../output.log 2> ../../error.log" );
+
+task.stdout.on( "data",
+	function onData( data ){
+		console.log( data );
+	} );
+
+task.stderr.on( "data",
+	function onError( data ){
+		console.error( data );
+	} );
