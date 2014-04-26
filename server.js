08b70e454b00e2c285e7f864f56b746aae2a1861
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-26 13:45:19 +0800

    Added npm install command.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var childprocess = require( "child_process" );
-var task = childprocess.exec( "cd ./sails/webnified-app/ && npm start --prod 1> ../../output.log 2> ../../error.log",
+var task = childprocess.exec( "cd ./sails/webnified-app/ && npm install && npm start --prod 1> ../../output.log 2> ../../error.log",
 	{
 		"env": process.env
 	} );
